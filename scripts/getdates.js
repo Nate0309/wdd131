59dbb9ead584216ad70696b8a6f79f20b54d0eb8
Nate0309 <omoorenathan848@gmail.com>
2025-03-13 16:52:17 +0200

    editted date

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,6 +1,6 @@
 const currentyear = document.querySelector("#currentyear");
 const year = new Date();
-currentyear.innerHTML = year;
+currentyear.innerHTML = year.getFullYear();
 
 var latestDate = new Date();
 document.getElementById("lastModified").innerHTML = latestDate;
