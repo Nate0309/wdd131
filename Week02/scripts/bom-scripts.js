da750ea292793727f206f8217cac030a3afe0237
Nate0309 <omoorenathan848@gmail.com>
2025-03-13 13:51:46 +0200

    javascripte updated to add events

--- Week02/scripts/bom-scripts.js
+++ Week02/scripts/bom-scripts.js
@@ -1,11 +1,30 @@
 const input = document.querySelector('#favchap');
 const button = document.querySelector('button');
-const list = document.querySelector('list');
+const list = document.querySelector('#list');
 
-const li = document.createElement('li');
-const deleteButton = document.createElement('button');
 
-li.textContent = input.value;
-deleteButton.textContent = 'X';
-li.append(deleteButton);
-list.append(li);
+
+
+button.addEventListener('click', function(){
+    if (input.value.trim() === ''){
+        input.value = "Please enter a chapter before submitting";
+        input.focus();
+        return;
+    }
+    const li = document.createElement('li');
+    const deleteButton = document.createElement('button');
+    li.textContent = input.value;
+    deleteButton.textContent = 'X';
+    li.append(deleteButton);
+    list.append(li);
+
+    input.focus();
+    input.value = "";
+
+    deleteButton.addEventListener('click', function(){
+        list.removeChild(li);
+        input.focus();
+        input.value = "";
+    })
+})
+
